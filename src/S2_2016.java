4ca6e0a7ec973f71507e50b6c5b437f1109b137f
Michael Gutman <michaelgutman@hotmail.com>
2017-01-10 18:21:34 -0500

    2016 s2 Tandem bicycle solution

--- src/S2_2016.java
+++ src/S2_2016.java
@@ -1,9 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class S2_2016 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		int q = 0, n= 0;
+		String[] dS = null, pS = null;
+ 		try {
+			q = Integer.parseInt(r.readLine());
+			n = Integer.parseInt(r.readLine());
+			dS = r.readLine().split(" ");
+			pS = r.readLine().split(" ");
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+ 		
+ 		int[] d = new int[n];
+ 		int[] p = new int[n];
+ 		
+ 		for (int i = 0; i < n; i++) {
+ 			d[i] = Integer.parseInt(dS[i]);
+ 			p[i] = Integer.parseInt(pS[i]);
+ 		}
+ 		
+ 		Arrays.sort(d);
+ 		Arrays.sort(p);
+ 		
+ 		int sum = 0;
+ 		
+ 		switch (q) {
+ 		case 1:
+ 			for (int i = 0; i < n; i++) {
+ 				sum += Math.max(d[i],p[i]);
+ 			}
+ 			System.out.println(sum);
+ 			break;
+ 		case 2:
+ 			for (int i = 0; i < n; i++) {
+ 				sum += Math.max(d[i],p[n-i-1]);
+ 			}
+ 			System.out.println(sum);
+ 			break;
+ 		}
 	}
 
 }
